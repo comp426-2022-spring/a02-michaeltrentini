4079a77e57f4136a602ca290872f2ab69f20b924
Michael Trentini <mtren@live.unc.edu>
2022-04-25 17:01:57 -0400

    require

--- flips.js
+++ flips.js
@@ -1,5 +1,5 @@
 import { coinFlips, countFlips } from "./modules/coin.mjs" //import functions from coin.mjs
-import { createrequire } from 'module';
+import { createRequire } from 'module'; // import createRequire
 
 const require = createRequire(import.meta.url);
 const args = require("./node_modules/minimist")(process.argv.slice(2));
