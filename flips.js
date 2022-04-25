4a281519477f556c27f8a99f9dfd3241945a3be7
Michael Trentini <mtren@live.unc.edu>
2022-04-25 17:38:31 -0400

    a

--- flips.js
+++ flips.js
@@ -1,11 +1,8 @@
 import { coinFlips, countFlips } from "./modules/coin.mjs" //import functions from coin.mjs
-import { createRequire } from 'module'; // import createRequire
-
-const require = createRequire(import.meta.url);
 const args = require("./node_modules/minimist")(process.argv.slice(2));
 
 args['number'];
-var x = coinFlips(args.number || process.env.number || 1)
+const x = args.number || 1
 
 let flips = coinFlips(x);
 
--- guess-flip.js
+++ guess-flip.js
@@ -5,12 +5,12 @@ const require = createRequire(import.meta.url);
 const args = (require("./node_modules/minimist"))(process.argv.slice(2));
 
 args["call"];
-const call = args.call || 1;
+const call = args.call;
 
 if (call == "tails" || call == "heads") {
     console.log(flipACoin(call));
 }
-else if (call == undefined) {
+else if (call == null) {
     console.log("Error: no input.");
 }
 else {
