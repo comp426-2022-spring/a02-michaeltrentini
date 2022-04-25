656eedf6663743b47e368119c62c36ab30e74586
Michael Trentini <mtren@live.unc.edu>
2022-04-25 17:39:45 -0400

    wrong

--- flips.js
+++ flips.js
@@ -1,6 +1,8 @@
 import { coinFlips, countFlips } from "./modules/coin.mjs" //import functions from coin.mjs
 const args = require("./node_modules/minimist")(process.argv.slice(2));
+import { createRequire } from 'module'; // import createRequire
 
+const require = createRequire(import.meta.url);
 args['number'];
 const x = args.number || 1
 
