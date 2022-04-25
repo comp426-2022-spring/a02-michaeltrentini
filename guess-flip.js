a4859bbf11cd949f693262910d8cc180d9d1dfb0
Michael Trentini <mtren@live.unc.edu>
2022-04-25 17:19:14 -0400

    random

--- guess-flip.js
+++ guess-flip.js
@@ -3,12 +3,16 @@ import { createRequire } from 'module'; // import createRequire
 
 const require = createRequire(import.meta.url);
 const args = (require("./node_modules/minimist"))(process.argv.slice(2));
-const call = args["call"];
+
+args["call"];
+const call = args.call;
 
 if (call == "tails" || call == "heads") {
     console.log(flipACoin(call));
 }
-else {
+else if (call == null) {
     console.log("Error: no input.");
+}
+else {
     console.log("Usage: node guess-flip --call=[heads|tails]");
 }
